ef3541e9dff12dc9a060f84b6024aa1a024082ea
wwztraders <63957825+wwztraders@users.noreply.github.com>
2020-04-20 17:00:01 +0500

    files addess

--- js/myapp.js
+++ js/myapp.js
@@ -1,9 +1,120 @@
-
+var currentDocId = '';
 (()=>{
   $('#errorMsg').hide();
   $('#successMsg').hide();
   $('#dangerMsg').hide();
+  $('#detailsContainer').hide();
+  
 })();
+
+const showModal = (docId) => {
+  $('#changeModal').modal();
+  currentDocId = docId;
+}
+
+const isArrived = () =>{
+  let docRef = db.collection("shipments").doc(currentDocId);
+  docRef.set({
+    arrivalDate:moment().format('LLLL')
+  },{
+    merge:true
+  }).then(()=>{
+    getAllData();
+  $('#changeModal').modal('hide');
+  })
+}
+
+const changeStatus = () => {
+  let docRef = db.collection("shipments").doc(currentDocId);
+  docRef.set({
+    status:$('#statusDrpDwn').val()
+  },{
+    merge:true
+  }).then(()=>{
+    getAllData();
+  $('#changeModal').modal('hide');
+  })
+  
+
+}
+
+const searchCons = () => {
+  let toBeSearch = $('#consSearch').val();
+
+  db.collection("shipments").where('consName','==',toBeSearch.trim())
+  .get()
+  .then(function(querySnapshot) {
+    $('#detailsContainer').show();
+    let html = '';
+      querySnapshot.forEach(function(doc) {
+          html+=`<tr>
+          <td>${doc.data().departureDate}</td>
+          <td>${doc.data().consName}</td>
+          <td>${doc.data().address}</td>
+          <td>${doc.id}</td>
+          <td>${doc.data().name}</td>
+          <td><span class="badge badge-info" style="cursor:pointer;" onclick="showModal('${doc.id}')">${doc.data().status}</span></td>
+          <td>${doc.data().details}</td>
+          </tr>`
+          $('#detailsContainer').html( `Consignment # <span>${doc.data().consName}</span>
+          <table>
+            <tr>
+              <td><b>Origin City</b></td>
+              <td>${doc.data().oCity}</td>
+            </tr>
+            <tr>
+              <td><b>Origin Country</b></td>
+              <td>${doc.data().oCountry}</td>
+            </tr>
+            <tr>
+              <td><b>Destination City</b></td>
+              <td>${doc.data().dCity}</td>
+            </tr>
+            <tr>
+              <td><b>Zip Code</b></td>
+              <td>${doc.data().zCode}</td>
+            </tr>
+            <tr>
+              <td><b>Destination Country</b></td>
+              <td>${doc.data().dCountry}</td>
+            </tr>
+            <tr>
+              <td><b>PCS</b></td>
+              <td>${doc.data().PCS}</td>
+            </tr>
+          </table>`);
+      });
+      
+        
+      $('#shipments').html(html);
+      
+});
+}
+
+const getAllData = ()=>{
+  db.collection("shipments")
+  .get()
+  .then(function(querySnapshot) {
+    let html = '';
+      querySnapshot.forEach(function(doc) {
+          html+=`<tr>
+          <td>${doc.data().departureDate}</td>
+          <td>${doc.data().consName}</td>
+          <td>${doc.data().address}</td>
+          <td>${doc.id}</td>
+          <td>${doc.data().name}</td>
+          <td><span class="badge badge-info" style="cursor:pointer;" onclick="showModal('${doc.id}')">${doc.data().status}</span></td>
+          <td>${doc.data().details}</td>
+          </tr>`
+          
+      });
+
+      $('#shipments').html(html);
+  })
+  .catch(function(error) {
+      console.log("Error getting documents: ", error);
+  });
+}
 
 var firebaseConfig = {
     apiKey: "AIzaSyAoUeB_mJJx0sAeJPSsnUb_fX6o8ruIs-Q",
@@ -18,6 +129,10 @@ var firebaseConfig = {
   // Initialize Firebase
   var defaultProject = firebase.initializeApp(firebaseConfig);
   var db = firebase.firestore();
+  getAllData();
+
+
+  
 
 const changeBtnStyle = (id,text)=>{
   let btn = $('#'+id);
@@ -43,7 +158,16 @@ $('#btnSave').click(()=>{
     phone: $('#number').val(),
     details:$('#details').val(),
     address:$('#address').val(),
-    date:new Date(),
+    departureDate:moment().format('LLLL'),
+    arrivalDate:'',
+    status:'Initiated',
+    consName:$('#consName').val(),
+    oCity:$('#oCity').val(),
+    oCountry:$('#oCountry').val(),
+    dCity:$('#dCity').val(),
+    zCode:$('#zCode').val(),
+    dCountry:$('#dCountry').val(),
+    PCS:$('#pcs').val()
 })
 .then(function() {
     revertBtnStyle('btnSave','Save');
@@ -64,6 +188,8 @@ $('#btnSave').click(()=>{
 })
 
 
+  
+
 $('#btnTrack').click(()=>{
   
 
@@ -73,7 +199,7 @@ $('#btnTrack').click(()=>{
     return;
   }
   changeBtnStyle('btnTrack','Tracking...')
-  let docRef = db.collection('shipments').doc($('#searchNumber').val())
+  let docRef = db.collection('shipments').doc($('#searchNumber').val().trim())
   docRef.get().then(function(doc) {
     if (doc.exists) {
       revertBtnStyle('btnTrack','Track')
@@ -83,27 +209,42 @@ $('#btnTrack').click(()=>{
       </div>
       <div class="card">
         <div class="card-header">
-          Tracking No# ${$('#searchNumber').val()}
+          Tracking No.  ${$('#searchNumber').val()}<h6 class="badge badge-info float-right">${doc.data().status}</h6>
         </div>
+       
+
         <div class="card-body">
-          
-          <h5 class="card-title">Customer Name : </h5>
+    
           <p class="card-text">
-          ${doc.data().name}
+            <div class="row">
+              
+              <div class="col-md-6">
+              <h6>Departure date and time </h6><span><p>${doc.data().departureDate}</p></span>
+              </div>
+              <div class="col-md-6">
+              <h6>Arrival date and time </h6><span><p>${doc.data().arrivalDate == ""?"---":doc.data().arrivalDate}</p></span>
+              </div>
+            </div>
           </p>
 
-          <h5 class="card-title">Phone No. : </h5>
-          <p class="card-text">
-          ${doc.data().phone}
-          </p>              
-          <h5 class="card-title">Address : </h5>
-          <p class="card-text" style="white-space:pre-line;">
-          ${doc.data().address}
-          </p>
-          <h5 class="card-title">Details : </h5>
-          <p class="card-text" style="white-space:pre-line;">
-          ${doc.data().details}
-          </p>
+          <div class="row">
+          <div class="col-md-6">
+                <h6>Customer name </h6><span><p>${doc.data().name}</p></span>
+              </div>
+              <div class="col-md-6">
+                <h6>Phone </h6><span><p>${doc.data().phone}</p></span>
+              </div>
+              
+            </div>
+
+            <div class="row">
+            <div class="col">
+              <h6>Location </h6><span><p>${doc.data().address}</p></span>
+              </div>
+            </div>
+            <div class="row">
+           
+            </div>
         </div>
       </div>`;
       $('#trackBody').html(html);
